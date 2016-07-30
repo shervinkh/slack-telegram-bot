2eff0ad2583c2ac8ba818f04f4ccc3c9c38bc51f
Shervin Khastoo <shervinkh145@gmail.com>
2016-07-30 17:00:30 +0430

    Bug fix

--- slack_to_telegram/views.py
+++ slack_to_telegram/views.py
@@ -11,15 +11,17 @@ def slack_notification(request, notification_url):
     try:
         channel_name = escape_markdown(request.POST['channel_name'])
         user_name = escape_markdown(request.POST['user_name'])
+        user_id = request.POST['user_id']
         text = escape_markdown(request.POST['text'])
 
-        message = '_{user}_ *in* #{channel}:\n{text}'
+        if user_id != 'USLACKBOT':
+            message = '*{user}* _in_ #{channel}:\n{text}'
 
-        message = message.format(user=user_name, channel=channel_name, text=text)
+            message = message.format(user=user_name, channel=channel_name, text=text)
 
-        forward_list = SlackToTelegram.objects.filter(url=notification_url).values_list('bot_token', 'chat_id')
-        for (bot_token, chat_id) in forward_list:
-            TelegramBot(bot_token).send_message(chat_id, message)
+            forward_list = SlackToTelegram.objects.filter(url=notification_url).values_list('bot_token', 'chat_id')
+            for (bot_token, chat_id) in forward_list:
+                TelegramBot(bot_token).send_message(chat_id, message)
 
         return HttpResponse(status=200)
     except Exception:
